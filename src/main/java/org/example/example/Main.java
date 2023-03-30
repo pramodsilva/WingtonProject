920741709493676c7c1c0d44c16f6506f30d0d60
pramodsilva <pramodsilva.uom@gmail.com>
2023-03-30 12:50:51 +0530

    initial commit2

--- src/main/java/org/example/example/Main.java
+++ src/main/java/org/example/example/Main.java
@@ -5,9 +5,9 @@ import org.example.dto.Child;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world Boss!");
         Child child = new Child();
-        child.setName("Elephant");
+        child.setName("Elephant24");
         child.setAge(5);
         System.out.println("This is my age: " + child.getAge() + "\nthis is my name: " + child.getName());
     }
