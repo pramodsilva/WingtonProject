9550a4d42b55d5e23771b5912b97f5aba13e0cc1
pramodsilva <pramodsilva.uom@gmail.com>
2023-03-30 19:57:09 +0530

    initial commit243

--- src/main/java/org/example/dto/Child.java
+++ src/main/java/org/example/dto/Child.java
@@ -1,22 +1,51 @@
 package org.example.dto;
 
+/**
+ * The type Child.
+ */
 public class Child {
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Sets age.
+     *
+     * @param age the age
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * The Name.
+     */
     String name;
+    /**
+     * The Age.
+     */
     int age;
 }
