3a942bb60b4bc5ee908371965f20a13499a1cb31
Pramod Silva <pramodsilva.uom@gmail.com>
2023-12-04 16:37:58 +0530

    first

--- src/main/java/org/example/example/Main.java
+++ src/main/java/org/example/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Boss!");
         Child child = new Child();
-        child.setName("Elephant24");
+        child.setName("Elephant24 - my first");
         child.setAge(5);
         System.out.println("This is my age: " + child.getAge() + "\nthis is my name: " + child.getName());
     }
